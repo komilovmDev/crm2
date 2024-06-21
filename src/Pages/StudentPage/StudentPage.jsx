2b46071dfcc671b5c852e704c3f65d92bbce24f2
demdevapi <73824592+Dark-dka@users.noreply.github.com>
2024-06-21 15:54:33 +0300

    awdwad

--- src/Pages/StudentPage/StudentPage.jsx
+++ src/Pages/StudentPage/StudentPage.jsx
@@ -3,11 +3,16 @@ import './Studentpage.css';
 import { IoChevronBack } from "react-icons/io5";
 import { TbUserPlus } from "react-icons/tb";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
-import { LuPlusSquare } from "react-icons/lu";  
+import { LuPlusSquare } from "react-icons/lu";
 import { Link } from 'react-router-dom';
 import imgs from './../../Assets/e207b97fee7d4c7d854fff1fc5e81cee.jpg';
 import bgimage1 from './../../Assets/Clients/OBJECTSONe.png';
-import bgimage2 from './../../Assets/Clients/OBJECTS.png'; 
+import bgimage2 from './../../Assets/Clients/OBJECTS.png';
+import piont from './../../Assets/Clients/Frame 366.png';
+import avarage from './../../Assets/Clients/Frame 367.png';
+import { RiFileCopyLine } from "react-icons/ri";
+import userprofilfile from './../../Assets/Clients/Group 313.png';
+import twitterqr from './../../Assets/Clients/twitterLogotype.png';
 
 export default function StudentPage() {
     const [activeSection, setActiveSection] = useState('Profile');
@@ -18,6 +23,9 @@ export default function StudentPage() {
                 return (
                     <div className="Profile">
                         <div className="UserProfile">
+                            <div className="UserProfilRight">
+                                <img src={userprofilfile} alt="" />
+                            </div>
                             <div className="UserProfileLeft">
                                 <div className="UserProfileAvatars">
                                     <img src={imgs} alt="User Avatar" />
@@ -35,13 +43,16 @@ export default function StudentPage() {
                                 <div className="bguserprofilcard"></div>
                                 <div className="bgimage1"><img src={bgimage1} alt="" /></div>
                                 <div className="bgimage2"><img src={bgimage2} alt="" /></div>
-                                <div className="CardId">
-                                    <h2>Finance Card</h2>
-                                    <p>ID: <span>0989736</span></p>
-                                </div>
-                                <div className="CardBalance">
-                                    <p>Current balance:</p>
-                                    <h2>557,000 so’m</h2>
+
+                                <div className="UserTextProfile">
+                                    <div className="CardId">
+                                        <h2>Finance Card</h2>
+                                        <p>ID: <span>0989736</span></p>
+                                    </div>
+                                    <div className="CardBalance">
+                                        <p>Current balance:</p>
+                                        <h2>557,000 so’m</h2>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -49,21 +60,22 @@ export default function StudentPage() {
                             <div className="UserPointsUp">
                                 <div className="UserPointUpNav">
                                     <p>Points</p>
-                                    <span>Added to system: 23.05.2024</span>
-                                </div>
-                                <div className="UserPointsBox">
-                                    <div className="Points">
+                                    <div className="Pointss">
                                         <div className="PointsAva">
-                                            <img src="path_to_points_avatar" alt="Points Avatar" />
+                                            <img src={piont} alt="Points Avatar" />
                                         </div>
                                         <div className="PointsText">
                                             <span>160</span>
                                             <p>Points</p>
                                         </div>
                                     </div>
+
+                                </div>
+                                <div className="UserPointsBox">
+                                    <p>Added to system: 23.05.2024</p>
                                     <div className="Average">
                                         <div className="AverageAva">
-                                            <img src="path_to_average_avatar" alt="Average Avatar" />
+                                            <img src={avarage} alt="Average Avatar" />
                                         </div>
                                         <div className="AverageText">
                                             <span>4</span>
@@ -73,7 +85,19 @@ export default function StudentPage() {
                                 </div>
                             </div>
                             <div className="UserPointsQR">
-                                {/* Add QR code content or image here */}
+                                <div className="Qrbox">
+                                    <img src={twitterqr} alt="" />
+                                </div>
+                                <div className="QrTextbox">
+                                    <div className="QrCopy">
+                                        <p>Qr code</p>
+                                        <span><RiFileCopyLine /></span>
+                                    </div>
+                                    <div className="QrAbout">
+                                        <p>Change qr code</p>
+                                        <p>Share with sms</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -97,30 +121,30 @@ export default function StudentPage() {
                         <Link to={'/Clients'}><button><IoChevronBack /></button></Link>
                     </div>
                     <div className="ButtonNavigationStud">
-                        <button 
-                            id='Profile' 
-                            className={activeSection === 'Profile' ? 'active' : ''} 
+                        <button
+                            id='Profile'
+                            className={activeSection === 'Profile' ? 'active' : ''}
                             onClick={() => setActiveSection('Profile')}
                         >
                             Profile
                         </button>
-                        <button 
-                            id='Lesson' 
-                            className={activeSection === 'Lesson & homework' ? 'active' : ''} 
+                        <button
+                            id='Lesson'
+                            className={activeSection === 'Lesson & homework' ? 'active' : ''}
                             onClick={() => setActiveSection('Lesson & homework')}
                         >
                             Lesson & Homework
                         </button>
-                        <button 
-                            id='Finance' 
-                            className={activeSection === 'Finance' ? 'active' : ''} 
+                        <button
+                            id='Finance'
+                            className={activeSection === 'Finance' ? 'active' : ''}
                             onClick={() => setActiveSection('Finance')}
                         >
                             Finance
                         </button>
-                        <button 
-                            id='History' 
-                            className={activeSection === 'History' ? 'active' : ''} 
+                        <button
+                            id='History'
+                            className={activeSection === 'History' ? 'active' : ''}
                             onClick={() => setActiveSection('History')}
                         >
                             History
