6e11327e12bbf56dfa15fb113f1326346ff21931
K A I N <73824592+Dark-dka@users.noreply.github.com>
2024-06-24 10:08:27 +0500

    srtyuth

--- src/Pages/StudentPage/StudentPage.jsx
+++ src/Pages/StudentPage/StudentPage.jsx
@@ -15,6 +15,12 @@ import userprofilfile from './../../Assets/Clients/Group 313.png';
 import twitterqr from './../../Assets/Clients/twitterLogotype.png';
 import { BiChalkboard } from "react-icons/bi";
 import { FiLoader } from "react-icons/fi";
+import { BiCoinStack } from "react-icons/bi";
+import { CiCalendar } from "react-icons/ci";
+import { PiHandCoins } from "react-icons/pi";
+import { LuUserSquare2 } from "react-icons/lu";
+import { LiaFileAltSolid } from "react-icons/lia";
+
 
 export default function StudentPage() {
     const [activeSection, setActiveSection] = useState('Profile');
@@ -116,49 +122,139 @@ export default function StudentPage() {
                                 </div>
                                 <div className="PropStats"></div>
                             </div>
-                            <div id='' className="Statsbox">
+                            <div id='Amount' className="Statsbox">
                                 <div className="boardStats"><span><FiLoader /></span></div>
                                 <div className="StatsBoxUp">
                                     <p>Amount to be paid monthly</p>
                                 </div>
                                 <div className="StatBoxText">
-                                    <h2>1 218 000 so’m</h2>
+                                    <h2>1 218 000 <p>so’m</p></h2>
                                     <p>for this month</p>
                                 </div>
                                 <div className="PropStats"></div>
                             </div>
-                            <div className="Statsbox">
-                                <div className="boardStats"><span><BiChalkboard /></span></div>
+                            <div id='Amount' className="Statsbox">
+                                <div className="boardStats"><span><BiCoinStack /></span></div>
                                 <div className="StatsBoxUp">
-                                    <p>Active courses</p>
+                                    <p>Paid for this month</p>
                                 </div>
                                 <div className="StatBoxText">
-                                    <h2>2</h2>
-                                    <p>Group and individual</p>
+                                    <h2>218 000 <p>so’m</p></h2>
+                                    <p>from 1 218 000 so’m</p>
                                 </div>
                                 <div className="PropStats"></div>
                             </div>
-                            <div className="Statsbox">
-                                <div className="boardStats"><span><BiChalkboard /></span></div>
+                            <div id='Amount' className="Statsbox">
+                                <div className="boardStats"><span><CiCalendar /></span></div>
                                 <div className="StatsBoxUp">
-                                    <p>Active courses</p>
+                                    <p>Debt for this month</p>
                                 </div>
-                                <div className="StatBoxText">
-                                    <h2>2</h2>
-                                    <p>Group and individual</p>
+                                <div id='Debt' className="StatBoxText">
+                                    <h2>1 000 000 <p>so’m</p></h2>
+                                    <p>from 1 218 000 so’m</p>
                                 </div>
                                 <div className="PropStats"></div>
                             </div>
-                            <div className="Statsbox">
-                                <div className="boardStats"><span><BiChalkboard /></span></div>
+                            <div id='Amount' className="Statsbox">
+                                <div className="boardStats"><span><PiHandCoins /></span></div>
                                 <div className="StatsBoxUp">
-                                    <p>Active courses</p>
+                                    <p>All debt</p>
                                 </div>
-                                <div className="StatBoxText">
-                                    <h2>2</h2>
-                                    <p>Group and individual</p>
+                                <div id='Debt' className="StatBoxText">
+                                    <h2>1 105 000 <p>so’m</p></h2>
+                                    <p>from all periods</p>
                                 </div>
                                 <div className="PropStats"></div>
+                            </div>
+                        </div>
+                        <div className="ContactsLeadImfo">
+                            <div className="Contacts">
+                                <div className="ContactsTitle">
+                                    <span><LuUserSquare2 /></span>
+                                    <p>Contacts</p>
+                                </div>
+                                <div className="ContactContant">
+                                    <div className="ContactsLeft">
+                                        <div className="ContactLeftPhone">
+                                            <h3>Phone number:</h3>
+                                            <p>+998 99 966 7363</p>
+                                        </div>
+                                        <div className="ContactLeftSecondary">
+                                            <h3>Secondary number:</h3>
+                                            <p>+998 91 914 65 50</p>
+                                        </div>
+                                        <div className="ContactLeftBirthday">
+                                            <h3>Birthday:</h3>
+                                            <p>28.02.2001</p>
+                                        </div>
+                                        <div className="ContactLeftGender">
+                                            <h3>Gender:</h3>
+                                            <p>Male</p>
+                                        </div>
+                                    </div>
+                                    <div className="ContactsRight">
+                                        <div className="ContactsRightAdress">
+                                            <h3>Adress:</h3>
+                                            <p> Adress name</p>
+                                        </div>
+                                        <div className="ContactsRightTelegram">
+                                            <h3>Telegram profile:</h3>
+                                            <p>username</p>
+                                        </div>
+                                        <div className="ContactsRightInstagram">
+                                            <h3>Instagram profile:</h3>
+                                            <p>username</p>
+                                        </div>
+                                        <div className="ContactsRightEmail">
+                                            <h3>E-mail:</h3>
+                                            <p>admin@gmail.com</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="LeadInfo">
+                                <div className="ContactsTitle">
+                                    <span><LiaFileAltSolid /> </span>
+                                    <p>Lead informations</p>
+                                </div>
+                                <div className="LeacContant">
+                                    <div className="ContactsLeft">
+                                        <div className="ContactLeftPhone">
+                                            <h3>Subject:</h3>
+                                            <p>General English: Beginner level</p>
+                                        </div>
+                                        <div className="ContactLeftSecondary">
+                                            <h3>Lesson type:</h3>
+                                            <p>Group</p>
+                                        </div>
+                                        <div className="ContactLeftBirthday">
+                                            <h3>Teacher:</h3>
+                                            <p>Mr.Alibek</p>
+                                        </div>
+                                        <div className="ContactLeftGender">
+                                            <h3>Lesson time:</h3>
+                                            <p>08:00 - 09:00</p>
+                                        </div>
+                                    </div>
+                                    <div className="ContactsRight">
+                                        <div className="ContactsRightAdress">
+                                            <h3>Added date:</h3>
+                                            <p> 02.28.2023</p>
+                                        </div>
+                                        <div className="ContactsRightTelegram">
+                                            <h3>Changed date :</h3>
+                                            <p> 01.06.2024</p>
+                                        </div>
+                                        <div className="ContactsRightInstagram">
+                                            <h3>Study dates:</h3>
+                                            <p>not aviable</p>
+                                        </div>
+                                        <div className="ContactsRightEmail">
+                                            <h3>Adversiting source: </h3>
+                                            <p id='InfoWeb'>Web site</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
